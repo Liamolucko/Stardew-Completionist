b22149f779e4c90b1b74a433cde2b1ff0af3ed84
Liam Murphy <liampm32@gmail.com>
2021-03-02 17:46:23 +1100

    Updates

--- client/index.1e0ff09e.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as r}from"./client.30718474.js";async function e(){this.redirect(302,"dashboard")}export default class extends s{constructor(s){super(),t(this,s,null,null,r,{})}}export{e as preload};
